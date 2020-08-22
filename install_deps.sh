fb232e27eb3acba3204971e5135f16fee923d927
ackao <3639365+ackao@users.noreply.github.com>
2020-08-22 19:33:50 -0400

    add pyyaml dep

--- install_deps.sh
+++ install_deps.sh
@@ -5,3 +5,4 @@ apt-get install -y gcc
 apt-get install -y cmake libgtest-dev libgmock-dev
 apt-get install -y clang clang-tidy clang-format
 apt-get install -y python3 python3-pip
+pip3 install pyyaml

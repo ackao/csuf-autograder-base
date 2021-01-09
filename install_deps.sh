11863ee6465d3f13d1dafb698ce110988081a18e
pinventado <pinventado@fullerton.edu>
2021-01-09 13:53:37 -0800

    Fix typo

--- install_deps.sh
+++ install_deps.sh
@@ -6,4 +6,4 @@ apt-get install -y cmake
 apt-get install -y clang clang-tidy clang-format
 apt-get install -y python3 python3-pip
 pip3 install pyyaml
-cd /tmp/ && git clone https://github.com/google/googletest && cd googletest && mkdir build && cd build && cmake .. -DCMAKE_CXX_STANDARD=17 && make && make install && rmf -rf /tmp/googletest
+cd /tmp/ && git clone https://github.com/google/googletest && cd googletest && mkdir build && cd build && cmake .. -DCMAKE_CXX_STANDARD=17 && make && make install && rm -rf /tmp/googletest

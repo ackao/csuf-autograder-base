1f47d5de1f2d5a30e2a44302c976b305eb9bc333
pinventado <pinventado@fullerton.edu>
2021-01-09 14:11:20 -0800

    Add libx11 support

--- install_deps.sh
+++ install_deps.sh
@@ -3,7 +3,7 @@
 apt-get update
 apt-get install -y gcc
 apt-get install -y cmake
-apt-get install -y clang clang-tidy clang-format
+apt-get install -y clang clang-tidy clang-format libx11-dev
 apt-get install -y python3 python3-pip
 pip3 install pyyaml
 cd /tmp/ && git clone https://github.com/google/googletest && cd googletest && mkdir build && cd build && cmake .. -DCMAKE_CXX_STANDARD=17 && make && make install && rm -rf /tmp/googletest
